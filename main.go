58460b4550294e6863c51e0e42109c02c84653c4
Monokaix <2536818783@qq.com>
2021-11-30 20:44:43 +0800

    add multi client

--- main.go
+++ main.go
@@ -6,27 +6,28 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
-	"strconv"
 	"sync"
 	"time"
 
-	"k8s.io/apimachinery/pkg/api/errors"
+	"client-k8s/pkg"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
+var (
+	workNum      = 150
+	watchWorkNum = 150
+	qps          = 100
+	burst        = 100
+	clientCount  = 150
+)
+
 func main() {
-	var (
-		workNum      = 1
-		watchWorkNum = 1
-		qps          = 200
-		burst        = 500
-	)
 	flag.IntVar(&workNum, "worker", workNum, "number of current list workers")
 	flag.IntVar(&watchWorkNum, "watchWorker", watchWorkNum, "number of current watch workers")
 	flag.IntVar(&qps, "qps", qps, "kube client qps")
 	flag.IntVar(&burst, "burst", burst, "kube client burst")
+	flag.IntVar(&clientCount, "client-count", clientCount, "kube client count")
 	flag.Parse()
 
 	rand.Seed(time.Now().UnixNano())
@@ -38,20 +39,16 @@ func main() {
 	}
 	config.QPS = float32(qps)
 	config.Burst = burst
-	// create the clientSet
-	clientSet, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err.Error())
-	}
-	list(clientSet, &waitGroup, workNum)
-	watch(clientSet, &waitGroup, watchWorkNum)
-
+	pkg.MakeClient(clientCount, config)
+	list(&waitGroup, workNum)
+	watch(&waitGroup, watchWorkNum)
 	waitGroup.Wait()
 }
 
-func list(clientSet *kubernetes.Clientset, wg *sync.WaitGroup, worker int) {
+func list(wg *sync.WaitGroup, worker int) {
 	for i := 0; i < worker; i++ {
 		wg.Add(1)
+		clientSet := pkg.GetClient(i, clientCount)
 		go func() {
 			for {
 				pods, err := clientSet.CoreV1().Pods("test").List(context.TODO(), metav1.ListOptions{})
@@ -63,39 +60,40 @@ func list(clientSet *kubernetes.Clientset, wg *sync.WaitGroup, worker int) {
 				// Examples for error handling:
 				// - Use helper functions like e.g. errors.IsNotFound()
 				// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-				namespace := "test"
-				n := rand.Intn(100)
-				pod := "test-" + strconv.Itoa(n)
-				_, err = clientSet.CoreV1().Pods(namespace).Get(context.TODO(), pod, metav1.GetOptions{})
-				if errors.IsNotFound(err) {
-					fmt.Printf("Pod %s in namespace %s not found\n", pod, namespace)
-				} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-					fmt.Printf("Error getting pod %s in namespace %s: %v\n",
-						pod, namespace, statusError.ErrStatus.Message)
-				} else if err != nil {
-					panic(err.Error())
-				} else {
-					fmt.Printf("Found pod %s in namespace %s\n", pod, namespace)
-				}
-
-				time.Sleep(10 * time.Millisecond)
+				//namespace := "test"
+				//n := rand.Intn(100)
+				//pod := "test-" + strconv.Itoa(n)
+				//_, err = clientSet.CoreV1().Pods(namespace).Get(context.TODO(), pod, metav1.GetOptions{})
+				//if errors.IsNotFound(err) {
+				//	fmt.Printf("Pod %s in namespace %s not found\n", pod, namespace)
+				//} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+				//	fmt.Printf("Error getting pod %s in namespace %s: %v\n",
+				//		pod, namespace, statusError.ErrStatus.Message)
+				//} else if err != nil {
+				//	panic(err.Error())
+				//} else {
+				//	fmt.Printf("Found pod %s in namespace %s\n", pod, namespace)
+				//}
+				//
+				// time.Sleep(time.Second)
 			}
 		}()
 	}
 }
 
-func watch(clientSet *kubernetes.Clientset, wg *sync.WaitGroup, worker int) {
+func watch(wg *sync.WaitGroup, worker int) {
 	for i := 0; i < worker; i++ {
 		wg.Add(1)
+		clientSet := pkg.GetClient(i, clientCount)
 		go func() {
-			watch, err := clientSet.CoreV1().Pods("test").Watch(context.TODO(), metav1.ListOptions{})
+			_, err := clientSet.CoreV1().Pods("test").Watch(context.TODO(), metav1.ListOptions{})
 			if err != nil {
 				panic(err.Error())
 			}
-			select {
-			case e := <-watch.ResultChan():
-				fmt.Println(e)
-			}
+			//select {
+			//case _ = <-watch.ResultChan():
+			//	fmt.Println("got one event")
+			//}
 		}()
 	}
 }
---
+++ pkg/client.go
@@ -0,0 +1,23 @@
+package pkg
+
+import (
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
+
+var ClientSets []*kubernetes.Clientset
+
+func MakeClient(count int, config *rest.Config) {
+	ClientSets = make([]*kubernetes.Clientset, count)
+	for i := 0; i < count; i++ {
+		client, err := kubernetes.NewForConfig(config)
+		if err != nil {
+			panic(err)
+		}
+		ClientSets[i] = client
+	}
+}
+
+func GetClient(i, count int) *kubernetes.Clientset {
+	return ClientSets[i%count]
+}
